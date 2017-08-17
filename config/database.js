2d9013d74ce1ad0d590d81ad2638655d4991b8f2
ks212 <(none)>
2017-08-17 16:52:54 -0400

    mongoose introduced

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports={
+	uri:'mongodb://localhost:27017/' + this.db,
+	secret: '',
+	db: 'mean-angular-2'
+}
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var config = require('./config/database');
+
+mongoose.connect(config.uri,function(err){
+	if (err){
+		console.log('couldnot connect the database' + config.db);
+	}
+	else{
+		console.log('connected to database' + config.db);
+	}
+});
 
 app.get('/', function(req, res){
   res.send('hello world');
